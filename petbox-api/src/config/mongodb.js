e96c30e90ac747c0ec9346ba9058af3f88735e3b
Tiny Devin <trungvan.me@gmail.com>
2025-04-30 17:16:53 +0700

    Connect to mongodb atlas

--- petbox-api/src/config/mongodb.js
+++ petbox-api/src/config/mongodb.js
@@ -1,5 +1,25 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import { MongoClient, ServerApiVersion } from 'mongodb'
+import 'dotenv/config'
+const MONGODB_URI = process.env.MONGODB_URI
+const DATABASE_NAME = process.env.DATABASE_NAME
+
+let petboxDatabase = null
+const mongoClient = new MongoClient(MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+export const CONNECT_DB = async () => {
+  await mongoClient.connect()
+  petboxDatabase = mongoClient.db(DATABASE_NAME)
+}
+
+export const GET_DB = () => {
+  if (!petboxDatabase) {
+    throw new Error('Must connect to Database first!')
+  }
+  return petboxDatabase
+}
--- petbox-api/src/server.js
+++ petbox-api/src/server.js
@@ -1,14 +1,29 @@
+/* eslint-disable no-console */
 import express from 'express'
+import { CONNECT_DB } from '~/config/mongodb'
+import 'dotenv/config'
 
-const app = express()
+const PORT = process.env.APP_PORT || 8017
+const HOSTNAME = process.env.APP_HOST || 'localhost'
 
-const hostname = 'localhost'
-const port = 8017
+const START_SERVER = () => {
+  const app = express()
+  app.get('/', (req, res) => {
+    res.send('Server is running ...')
+  })
+  app.listen(PORT, HOSTNAME, () => {
+    console.log(`I am running at http://${HOSTNAME}:${PORT}/`)
+  })
+}
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
-
-app.listen(port, hostname, () => {
-  console.log(`I am running at http://${hostname}:${port}/`)
-})
+(async () => {
+  try {
+    console.log('-> Connecting to database ...')
+    await CONNECT_DB()
+    console.log('-> Connected to database successfully!')
+    START_SERVER()
+  } catch (error) {
+    console.error(error)
+    process.exit(0)
+  }
+})()
