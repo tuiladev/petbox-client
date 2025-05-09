53e8759eed9f86dbd2b78ab417735607ffe2716b
Tiny Devin <trungvan.me@gmail.com>
2025-05-09 22:03:42 +0700

    update env

--- src/utils/enviroment.js
+++ src/utils/enviroment.js
@@ -1,4 +1,7 @@
-export const env = {
-  API_ROOT: import.meta.env.VITE_API_ROOT,
-  API_VERSION: import.meta.env.VITE_API_VERSION
-}
+const API_ROOT = import.meta.env.DEV
+  ? 'http://localhost:8017'
+  : 'https://petbox-api-e4a2.onrender.com'
+
+const API_VERSION = 'v1'
+
+export const env = { API_ROOT, API_VERSION }
