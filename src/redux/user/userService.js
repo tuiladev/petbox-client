cf192f53199235fd59852599b3bc5cfdf4e37aa4
Tiny Devin <trungvan.me@gmail.com>
2025-05-10 10:26:06 +0700

    update redirect_uri

--- src/redux/user/userService.js
+++ src/redux/user/userService.js
@@ -4,7 +4,7 @@ import { env } from '~/utils/enviroment'
 export const googleLoginAPI = async (data) => {
   const response = await authorizedAxiosInstance.post(
     `${env.API_ROOT}/${env.API_VERSION}/users/google-login`,
-    data
+    { code: data.code }
   )
   return response.data
 }
